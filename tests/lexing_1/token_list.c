dad3f76191174d528e5bdb26773bf12fa91c2b15
DanielSurf10 <daniel101surf@gmail.com>
2024-04-13 23:35:26 -0300

    nova lógica lexing 1: só o token word tem uma string para o lexema

--- tests/lexing_1/token_list.c
+++ tests/lexing_1/token_list.c
@@ -6,7 +6,7 @@
 /*   By: danbarbo <danbarbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 16:27:38 by danbarbo          #+#    #+#             */
-/*   Updated: 2024/04/09 00:01:15 by danbarbo         ###   ########.fr       */
+/*   Updated: 2024/04/13 23:32:36 by danbarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static t_token_list	*token_create_node(char *lexeme, int token_type)
 	t_token_list	*new_token;
 
 	new_token = malloc(sizeof(t_token_list));
-	new_token->token.content = lexeme;
+	new_token->token.lexeme = lexeme;
 	new_token->token.type = token_type;
 	new_token->next = NULL;
 	return (new_token);
@@ -49,7 +49,8 @@ void	token_clear_list(t_token_list **token_list)
 		while (aux1)
 		{
 			aux2 = aux1->next;
-			free(aux1->token.content);
+			if (aux1->token.lexeme)
+				free(aux1->token.lexeme);
 			free(aux1);
 			aux1 = aux2;
 		}
