d263e8e024e845b053798a935b6ce158b8e7ac73
Daniel Barbosa Alves <danbarbo@c2r10p4.42sp.org.br>
2024-05-02 20:24:28 -0300

    execve incompleto

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: danbarbo <danbarbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 15:43:25 by danbarbo          #+#    #+#             */
-/*   Updated: 2024/05/02 12:38:09 by danbarbo         ###   ########.fr       */
+/*   Updated: 2024/05/02 20:21:07 by danbarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,23 @@ void	print_tree(t_exec_tree *exec_tree)
 
 int	exec_cmd(t_token_list *command)
 {
-	printf("%s\n", command->token.lexeme);
+	int	pid;
+	int	ret_code;
 
-	if (ft_strncmp("ping", command->token.lexeme, -1) == 0)
-		return (1);
+	char	*cmd = command->token.lexeme;
+	char	*argv[] = {command->token.lexeme, command->next->token.lexeme, NULL};
 
-	return (0);
+	pid = fork();
+
+	if (pid == 0)
+	{
+		execve(cmd, argv, __environ);
+		exit(1);
+	}
+
+	waitpid(pid, &ret_code, 0);
+
+	return (ret_code);
 }
 
 int	exec_and(t_exec_tree *tree)
@@ -89,6 +100,7 @@ int	main()
 	int			ret_code;
 	t_exec_tree	*tree;
 
+	// && e ||
 
 // 	// First branch
 // 	tree = malloc(sizeof(t_exec_tree));
@@ -122,89 +134,112 @@ int	main()
 // 	tree->left->right->right = NULL;
 
 
-	// primeiro
+	// Sub Shell
+
+// 	// primeiro
+// 	tree = malloc(sizeof(t_exec_tree));
+// 	tree->type = AND;
+// 	// tree->command = get_token_list("&&");
+// 	tree->command = NULL;
+//
+// 	// esquerda
+// 	tree->left = malloc(sizeof(t_exec_tree));
+// 	tree->left->type = AND;
+// 	// tree->left->command = get_token_list("&&");
+// 	tree->left->command = NULL;
+//
+// 	// direita
+// 	tree->right = malloc(sizeof(t_exec_tree));
+// 	tree->right->type = COMMAND;
+// 	tree->right->command = get_token_list("/bin/echo 1");
+// 	tree->right->left = NULL;
+// 	tree->right->right = NULL;
+//
+// 	// esquerda esquerda
+// 	tree->left->left = malloc(sizeof(t_exec_tree));
+// 	tree->left->left->type = COMMAND;
+// 	tree->left->left->command = get_token_list("/bin/echo 2");
+// 	tree->left->left->left = NULL;
+// 	tree->left->left->right = NULL;
+//
+// 	// esquerda direita
+// 	tree->left->right = malloc(sizeof(t_exec_tree));
+// 	tree->left->right->type = SUBSHELL;
+// 	tree->left->right->command = NULL;
+// 	tree->left->right->left = NULL;
+// 	tree->left->right->right = NULL;
+//
+// 	// esquerda direita subshell
+// 	tree->left->right->subshell = malloc(sizeof(t_exec_tree));
+// 	tree->left->right->subshell->type = AND;
+// 	// tree->left->right->subshell->command = get_token_list("&&");
+// 	tree->left->right->subshell->command = NULL;
+//
+// 	// esquerda direita subshell esquerda
+// 	tree->left->right->subshell->left = malloc(sizeof(t_exec_tree));
+// 	tree->left->right->subshell->left->type = AND;
+// 	// tree->left->right->subshell->left->command = get_token_list("&&");
+// 	tree->left->right->subshell->left->command = NULL;
+//
+// 	// esquerda direita subshell direita
+// 	tree->left->right->subshell->right = malloc(sizeof(t_exec_tree));
+// 	tree->left->right->subshell->right->type = COMMAND;
+// 	tree->left->right->subshell->right->command = get_token_list("/bin/echo 3");
+//
+// 	// esquerda direita subshell esquerda esquerda
+// 	tree->left->right->subshell->left->left = malloc(sizeof(t_exec_tree));
+// 	tree->left->right->subshell->left->left->type = AND;
+// 	// tree->left->right->subshell->left->left->command = get_token_list("&&");
+// 	tree->left->right->subshell->left->left->command = NULL;
+//
+// 	// esquerda direita subshell esquerda direita
+// 	tree->left->right->subshell->left->right = malloc(sizeof(t_exec_tree));
+// 	tree->left->right->subshell->left->right->type = SUBSHELL;
+// 	tree->left->right->subshell->left->right->command = NULL;
+// 	tree->left->right->subshell->left->right->left = NULL;
+// 	tree->left->right->subshell->left->right->right = NULL;
+//
+// 	// esquerda direita subshell esquerda direita subshell
+// 	tree->left->right->subshell->left->right->subshell = malloc(sizeof(t_exec_tree));
+// 	tree->left->right->subshell->left->right->subshell->type = COMMAND;
+// 	tree->left->right->subshell->left->right->subshell->command = get_token_list("/bin/echo 4");
+// 	tree->left->right->subshell->left->right->subshell->left = NULL;
+// 	tree->left->right->subshell->left->right->subshell->right = NULL;
+//
+// 	// esquerda direita subshell esquerda esquerda esquerda
+// 	tree->left->right->subshell->left->left->left = malloc(sizeof(t_exec_tree));
+// 	tree->left->right->subshell->left->left->left->type = COMMAND;
+// 	tree->left->right->subshell->left->left->left->command = get_token_list("/bin/echo 5");
+// 	tree->left->right->subshell->left->left->left->left = NULL;
+// 	tree->left->right->subshell->left->left->left->right = NULL;
+//
+// 	// esquerda direita subshell esquerda esquerda direita
+// 	tree->left->right->subshell->left->left->right = malloc(sizeof(t_exec_tree));
+// 	tree->left->right->subshell->left->left->right->type = COMMAND;
+// 	tree->left->right->subshell->left->left->right->command = get_token_list("/bin/echo 6");
+// 	tree->left->right->subshell->left->left->right->left = NULL;
+// 	tree->left->right->subshell->left->left->right->right = NULL;
+
+
+	// Execve
+
 	tree = malloc(sizeof(t_exec_tree));
 	tree->type = AND;
-	// tree->command = get_token_list("&&");
 	tree->command = NULL;
 
-	// esquerda
+	// Esquerda
 	tree->left = malloc(sizeof(t_exec_tree));
-	tree->left->type = AND;
-	// tree->left->command = get_token_list("&&");
-	tree->left->command = NULL;
+	tree->left->type = COMMAND;
+	tree->left->command = get_token_list("/bin/ls -l");
+	tree->left->left = NULL;
+	tree->left->right = NULL;
 
-	// direita
+	// Direita
 	tree->right = malloc(sizeof(t_exec_tree));
 	tree->right->type = COMMAND;
-	tree->right->command = get_token_list("pwd");
+	tree->right->command = get_token_list("/bin/cat todo");
 	tree->right->left = NULL;
 	tree->right->right = NULL;
-
-	// esquerda esquerda
-	tree->left->left = malloc(sizeof(t_exec_tree));
-	tree->left->left->type = COMMAND;
-	tree->left->left->command = get_token_list("pwd");
-	tree->left->left->left = NULL;
-	tree->left->left->right = NULL;
-
-	// esquerda direita
-	tree->left->right = malloc(sizeof(t_exec_tree));
-	tree->left->right->type = SUBSHELL;
-	tree->left->right->command = NULL;
-	tree->left->right->left = NULL;
-	tree->left->right->right = NULL;
-
-	// esquerda direita subshell
-	tree->left->right->subshell = malloc(sizeof(t_exec_tree));
-	tree->left->right->subshell->type = AND;
-	// tree->left->right->subshell->command = get_token_list("&&");
-	tree->left->right->subshell->command = NULL;
-
-	// esquerda direita subshell esquerda
-	tree->left->right->subshell->left = malloc(sizeof(t_exec_tree));
-	tree->left->right->subshell->left->type = AND;
-	// tree->left->right->subshell->left->command = get_token_list("&&");
-	tree->left->right->subshell->left->command = NULL;
-
-	// esquerda direita subshell direita
-	tree->left->right->subshell->right = malloc(sizeof(t_exec_tree));
-	tree->left->right->subshell->right->type = COMMAND;
-	tree->left->right->subshell->right->command = get_token_list("pwd");
-
-	// esquerda direita subshell esquerda esquerda
-	tree->left->right->subshell->left->left = malloc(sizeof(t_exec_tree));
-	tree->left->right->subshell->left->left->type = AND;
-	// tree->left->right->subshell->left->left->command = get_token_list("&&");
-	tree->left->right->subshell->left->left->command = NULL;
-
-	// esquerda direita subshell esquerda direita
-	tree->left->right->subshell->left->right = malloc(sizeof(t_exec_tree));
-	tree->left->right->subshell->left->right->type = SUBSHELL;
-	tree->left->right->subshell->left->right->command = NULL;
-	tree->left->right->subshell->left->right->left = NULL;
-	tree->left->right->subshell->left->right->right = NULL;
-
-	// esquerda direita subshell esquerda direita subshell
-	tree->left->right->subshell->left->right->subshell = malloc(sizeof(t_exec_tree));
-	tree->left->right->subshell->left->right->subshell->type = COMMAND;
-	tree->left->right->subshell->left->right->subshell->command = get_token_list("cd ..");
-	tree->left->right->subshell->left->right->subshell->left = NULL;
-	tree->left->right->subshell->left->right->subshell->right = NULL;
-
-	// esquerda direita subshell esquerda esquerda esquerda
-	tree->left->right->subshell->left->left->left = malloc(sizeof(t_exec_tree));
-	tree->left->right->subshell->left->left->left->type = COMMAND;
-	tree->left->right->subshell->left->left->left->command = get_token_list("cd ..");
-	tree->left->right->subshell->left->left->left->left = NULL;
-	tree->left->right->subshell->left->left->left->right = NULL;
-
-	// esquerda direita subshell esquerda esquerda direita
-	tree->left->right->subshell->left->left->right = malloc(sizeof(t_exec_tree));
-	tree->left->right->subshell->left->left->right->type = COMMAND;
-	tree->left->right->subshell->left->left->right->command = get_token_list("pwd");
-	tree->left->right->subshell->left->left->right->left = NULL;
-	tree->left->right->subshell->left->left->right->right = NULL;
 
 
 	ret_code = exec_tree(tree);
