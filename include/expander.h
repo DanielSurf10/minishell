da394b17da2a7593625e722a6b017b8dd1ff0f5b
Leonardo Barbosa Sales <leobarbo@c3r7p1.42sp.org.br>
2024-05-30 16:25:10 -0300

    Norminettado

--- include/expander.h
+++ include/expander.h
@@ -6,7 +6,7 @@
 /*   By: leobarbo <leobarbo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 17:07:05 by leobarbo          #+#    #+#             */
-/*   Updated: 2024/05/30 16:12:31 by leobarbo         ###   ########.fr       */
+/*   Updated: 2024/05/30 16:24:18 by leobarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,16 @@
 
 typedef struct s_envp_list
 {
-	char			*key;
-	char			*value;
+	char				*key;
+	char				*value;
 	struct s_envp_list	*next;
-}					t_envp_list;
+}	t_envp_list;
 
 typedef struct s_expand_list
 {
 	char					letter;
 	struct s_expand_list	*next;
-}					t_str_lst;
+}	t_str_lst;
 
 //****************************************************************************//
 //                                 Functions                                  //
@@ -47,41 +47,41 @@ typedef struct s_expand_list
 //****************************************************************************//
 
 t_envp_list	*env_create_node(char *key, char *value);
-void	env_insert_node(t_envp_list **head, char *key, char *value);
+void		env_insert_node(t_envp_list **head, char *key, char *value);
 t_envp_list	*store_to_list(char *envp[]);
-char	**create_envp(t_envp_list *head);
-void	env_delete_value(t_envp_list **head, char *key);
+char		**create_envp(t_envp_list *head);
+void		env_delete_value(t_envp_list **head, char *key);
 
 //****************************************************************************//
 //                               expand_string                                //
 //****************************************************************************//
 
-char	*expand_string(char *str, t_envp_list *head);
+char		*expand_string(char *str, t_envp_list *head);
 
 //****************************************************************************//
 //                                string_list                                 //
 //****************************************************************************//
 
-void	add_letter_list(t_str_lst **lst, char letter);
-int		size_list(t_str_lst *lst);
-void	add_string_to_list(t_str_lst **lst, char *str);
-void	clear_str_list(t_str_lst **head);
-char	*create_string_from_list(t_str_lst *lst);
+void		add_letter_list(t_str_lst **lst, char letter);
+int			size_list(t_str_lst *lst);
+void		add_string_to_list(t_str_lst **lst, char *str);
+void		clear_str_list(t_str_lst **head);
+char		*create_string_from_list(t_str_lst *lst);
 
 //****************************************************************************//
 //                               utils_envp_list                              //
 //****************************************************************************//
 
-void	clear_list(t_envp_list **head);
-int		att_existing_value(t_envp_list *head, char *key, char *value);
-char	*search_value(t_envp_list *head, char *key);
-int		envp_list_size(t_envp_list *head);
-void	free_envp(char **envp);
+void		clear_list(t_envp_list **head);
+int			att_existing_value(t_envp_list *head, char *key, char *value);
+char		*search_value(t_envp_list *head, char *key);
+int			envp_list_size(t_envp_list *head);
+void		free_envp(char **envp);
 
 //****************************************************************************//
 //                                    utils                                   //
 //****************************************************************************//
 
-int		ternary(int condition, int if_true, int if_false);
+int			ternary(int condition, int if_true, int if_false);
 
 #endif
