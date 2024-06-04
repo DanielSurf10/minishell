7bcfcf22bb8daab680fdc06c6bdb5c505bcee1e5
DanielSurf10 <71985055+DanielSurf10@users.noreply.github.com>
2024-06-03 23:30:39 -0300

    expand_string_here_doc norminetado

--- src/expander/expand_string_here_doc.c
+++ src/expander/expand_string_here_doc.c
@@ -6,18 +6,57 @@
 /*   By: danbarbo <danbarbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 22:18:47 by danbarbo          #+#    #+#             */
-/*   Updated: 2024/06/03 23:12:02 by danbarbo         ###   ########.fr       */
+/*   Updated: 2024/06/03 23:28:02 by danbarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expander.h"
 
+static void	skip_invalid_variable(char *str, int *i)
+{
+	if (!(str[*i] >= '0' && str[*i] <= '9'))
+	{
+		while (is_valid_var(str[*i]))
+			(*i)++;
+		if (str[*i] == '$')
+			(*i)++;
+		if (is_valid_var(str[*i]) == 0)
+			(*i)--;
+	}
+}
+
+static void	*expand_variable_heredoc(char *str, int *i, t_str_list **new_str, \
+									t_envp_list *envp_list)
+{
+	int		k;
+	char	*key;
+	char	*expanded;
+
+	(*i)++;
+	if (str[*i] >= '0' && str[*i] <= '9' || str[*i] == '$'
+		|| is_valid_var(str[*i]) == 0)
+		skip_invalid_variable(str, i);
+	else
+	{
+		k = 0;
+		while (is_valid_var(str[*i]))
+		{
+			k++;
+			(*i)++;
+		}
+		key = ft_substr(str, (*i) - k, k);
+		expanded = search_value(envp_list, key);
+		add_string_to_list(new_str, expanded);
+		free(key);
+		free(expanded);
+		(*i)--;
+	}
+}
+
 char	*expand_string_heredoc(char *str, t_envp_list *envp_list)
 {
 	int			i;
-	int			k;
 	int			in_quotes;
-	char		*key;
 	char		*expanded;
 	t_str_list	*new_str;
 
@@ -31,45 +70,7 @@ char	*expand_string_heredoc(char *str, t_envp_list *envp_list)
 		else if (str[i] == '\"' && (in_quotes == 0 || in_quotes == 2))
 			in_quotes = ternary(in_quotes == 0, 2, 0);
 		else if (str[i] == '$' && in_quotes != 1)
-		{
-
-			// Isso aqui pode dar uma função separada
-			// Começa aqui
-
-			i++;
-			if (str[i] >= '0' && str[i] <= '9' || str[i] == '$'
-				|| is_valid_var(str[i]) == 0)
-			{
-				if (!(str[i] >= '0' && str[i] <= '9'))
-				{
-					while (is_valid_var(str[i]))
-						i++;
-					if (str[i] == '$')
-						i++;
-					// if (str[i] == ' ' || str[i] == '\'' || str[i] == '"')
-					if (is_valid_var(str[i]) == 0)
-						i--;
-				}
-			}
-			else
-			{
-				k = 0;
-				while (is_valid_var(str[i]))
-				{
-					k++;
-					i++;
-				}
-				key = ft_substr(str, i - k, k);
-				expanded = search_value(envp_list, key);
-				add_string_to_list(&new_str, expanded);
-				free(key);
-				free(expanded);
-				i--;
-			}
-
-			// Termina aqui
-
-		}
+			expand_variable_heredoc(str, &i, &new_str, envp_list);
 		else
 			add_letter_list(&new_str, str[i]);
 		i++;
