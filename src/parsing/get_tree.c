058bbe58cb7fe25f6a1d330576ddb879aa0d13e8
Leonardo Barbosa Sales <leobarbo@c1r4p3.42sp.org.br>
2024-06-11 20:44:18 -0300

    get_tree norminetado

--- src/parsing/get_tree.c
+++ src/parsing/get_tree.c
@@ -6,7 +6,7 @@
 /*   By: leobarbo <leobarbo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 17:44:55 by danbarbo          #+#    #+#             */
-/*   Updated: 2024/06/11 19:19:27 by leobarbo         ###   ########.fr       */
+/*   Updated: 2024/06/11 20:42:14 by leobarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,11 +65,10 @@ t_exec_tree	*get_tree(t_token_list *token_list, t_minishell *data)
 	t_exec_tree		*tree;
 	t_token_list	*inverted_list;
 
-	inverted_list = invert_list(token_get_sublist(token_list, 0, token_list_size(token_list)));
-
+	inverted_list = invert_list(token_get_sublist(token_list, 0, \
+			token_list_size(token_list)));
 	tree = make_tree(inverted_list, data);
 	token_clear_list(&inverted_list);
-
 	return (tree);
 }
 
@@ -77,19 +76,14 @@ void	*free_tree(t_exec_tree **tree)
 {
 	if (*tree == NULL)
 		return (NULL);
-
 	if ((*tree)->subshell)
 		free_tree(&(*tree)->subshell);
-
 	if ((*tree)->left)
 		free_tree(&(*tree)->left);
-
 	if ((*tree)->right)
 		free_tree(&(*tree)->right);
-
 	if ((*tree)->command)
 		token_clear_list(&(*tree)->command);
-
 	free(*tree);
 	*tree = NULL;
 	return (NULL);
@@ -99,23 +93,18 @@ void	*free_tree_all(t_exec_tree **tree)
 {
 	if (*tree == NULL)
 		return (NULL);
-
 	if ((*tree)->subshell)
 		free_tree_all(&(*tree)->subshell);
-
 	if ((*tree)->left)
 	{
 		if ((*tree)->type == REDIRECT_HEREDOC)
 			unlink((*tree)->left->command->token.lexeme);
 		free_tree_all(&(*tree)->left);
 	}
-
 	if ((*tree)->right)
 		free_tree_all(&(*tree)->right);
-
 	if ((*tree)->command)
 		token_clear_list(&(*tree)->command);
-
 	free(*tree);
 	*tree = NULL;
 	return (NULL);
