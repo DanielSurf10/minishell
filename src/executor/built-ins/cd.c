5b2cc5e215f105931784549ae1c15a1b4b5aeaac
Leonardo Barbosa Sales <leobarbo@c1r4p3.42sp.org.br>
2024-06-11 11:55:12 -0300

    Tentando norminettar o builtin

--- src/executor/built-ins/cd.c
+++ src/executor/built-ins/cd.c
@@ -6,69 +6,73 @@
 /*   By: leobarbo <leobarbo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 17:41:23 by leobarbo          #+#    #+#             */
-/*   Updated: 2024/06/01 18:51:19 by leobarbo         ###   ########.fr       */
+/*   Updated: 2024/06/11 11:36:50 by leobarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "executor.h"
 
-static int add_envp_list(t_envp_list **head, char *key, char *value)
+static	int	add_envp_list(t_envp_list **head, char *key, char *value)
 {
-    t_envp_list *new;
-    t_envp_list *tmp;
+	t_envp_list	*new;
+	t_envp_list	*tmp;
 
-    new = malloc(sizeof(t_envp_list));
-    if (new == NULL)
-        return (1);
-    new->key = ft_strdup(key);
-    new->value = ft_strdup(value);
-    new->next = NULL;
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        tmp = *head;
-        while (tmp->next)
-            tmp = tmp->next;
-        tmp->next = new;
-    }
-    return (0);
+	new = malloc(sizeof(t_envp_list));
+	if (new == NULL)
+		return (1);
+	new->key = ft_strdup(key);
+	new->value = ft_strdup(value);
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		tmp = *head;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+	return (0);
 }
 
-static int set_env(char *key, char *value, t_minishell *data)
+static	int	set_env(char *key, char *value, t_minishell *data)
 {
-    if (att_existing_value(data->envp_list, key, value) == 0)
-        add_envp_list(&data->envp_list, key, value);
-    return (0);
+	if (att_existing_value(data->envp_list, key, value) == 0)
+		add_envp_list(&data->envp_list, key, value);
+	return (0);
+}
+
+static	void	end_cd(char *path, char *oldpwd, t_minishell *data)
+{
+	free(path);
+	free(oldpwd);
+	set_env("OLDPWD", oldpwd, data);
+	set_env("PWD", getcwd(NULL, 0), data);
 }
 
 int	builtin_cd(char *args[], t_minishell *data)
 {
-    char	*path;
-    char	*oldpwd;
+	char	*path;
+	char	*oldpwd;
 
-    if (args[1] == NULL)
-        path = search_value(data->envp_list, "HOME");
-    else
-        path = ft_strdup(args[1]);
-    if (path == NULL)
-        return (1);
-    oldpwd = getcwd(NULL, 0);
-    if (oldpwd == NULL)
-    {
-        free(path);
-        return (1);
-    }
-    if (chdir(path) == -1)
-    {
-        free(path);
-        free(oldpwd);
-        return (1);
-    }
-    free(path);
-    set_env("OLDPWD", oldpwd, data);
-    free(oldpwd);
-    set_env("PWD", getcwd(NULL, 0), data);
-    return (0);
+	if (args[1] == NULL)
+		path = search_value(data->envp_list, "HOME");
+	else
+		path = ft_strdup(args[1]);
+	if (path == NULL)
+		return (1);
+	oldpwd = getcwd(NULL, 0);
+	if (oldpwd == NULL)
+	{
+		free(path);
+		return (1);
+	}
+	if (chdir(path) == -1)
+	{
+		free(path);
+		free(oldpwd);
+		return (1);
+	}
+	return (0);
 }
--- src/executor/built-ins/exec_builtin.c
+++ src/executor/built-ins/exec_builtin.c
@@ -6,11 +6,60 @@
 /*   By: leobarbo <leobarbo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 15:11:11 by danbarbo          #+#    #+#             */
-/*   Updated: 2024/06/07 17:35:34 by leobarbo         ###   ########.fr       */
+/*   Updated: 2024/06/11 11:54:38 by leobarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
+
+
+static	void	builtin_call(char *cmd, char **argv, t_minishell *data)
+{
+	if (ft_strncmp(cmd, "echo", -1) == 0)
+		builtin_echo(argv);
+	else if (ft_strncmp(cmd, "cd", -1) == 0)
+		builtin_cd(argv, data);
+	else if (ft_strncmp(cmd, "pwd", -1) == 0)
+		builtin_pwd();
+	else if (ft_strncmp(cmd, "export", -1) == 0)
+		builtin_export(argv, data);
+	else if (ft_strncmp(cmd, "unset", -1) == 0)
+		builtin_unset(argv, &data->envp_list);
+	else if (ft_strncmp(cmd, "env", -1) == 0)
+		builtin_env(data);
+	else if (ft_strncmp(cmd, "exit", -1) == 0)
+		builtin_exit(argv, data);
+
+}
+typedef struct s_builtin
+{
+	int		idx;
+	int		fd_redir;
+	int		args_num;
+	char	*cmd;
+	char	**argv;
+	char	**envp;
+}	t_builtin;
+
+static	void	init_exec_cmd_builtin(t_exec_tree *tree, t_minishell *data, int *fd_redir)
+{
+	char	*cmd;
+
+	cmd = expand_string(tree->left->command->token.lexeme, data->envp_list);
+	*fd_redir = open_redir(cmd, tree->type);
+	free(cmd);
+	if (fd_redir == -1)
+		{
+			perror(tree->left->command->token.lexeme);
+			return(1);
+		}
+		if (tree->type == REDIRECT_INPUT)
+			dup2(fd_redir, STDIN_FILENO);
+		else if (tree->type == REDIRECT_OUTPUT || tree->type == REDIRECT_OUTPUT_APPEND)
+			dup2(fd_redir, STDOUT_FILENO);
+		fd_redir = close_fd(fd_redir);
+		ret_code = exec_cmd_builtin(tree->right, data);
+}
 
 int	exec_cmd_builtin(t_exec_tree *tree, t_minishell *data)
 {
@@ -25,51 +74,34 @@ int	exec_cmd_builtin(t_exec_tree *tree, t_minishell *data)
 	ret_code = 0;
 	if (tree->type >= REDIRECT_INPUT && tree->type <= REDIRECT_OUTPUT_APPEND)
 	{
-		cmd = expand_string(tree->left->command->token.lexeme, data->envp_list);
-		fd_redir = open_redir(cmd, tree->type);	// Fazer o tratamento de erro do redirecionamento ambíguo com o wildcard
-		free(cmd);
-		if (fd_redir == -1)
-		{
-			perror(tree->left->command->token.lexeme);
-			return(1);
-		}
-
-		if (tree->type == REDIRECT_INPUT)	// Talvez hereredoc
-			dup2(fd_redir, STDIN_FILENO);
-		else if (tree->type == REDIRECT_OUTPUT || tree->type == REDIRECT_OUTPUT_APPEND)
-			dup2(fd_redir, STDOUT_FILENO);
-		fd_redir = close_fd(fd_redir);
-		ret_code = exec_cmd_builtin(tree->right, data);
+		// cmd = expand_string(tree->left->command->token.lexeme, data->envp_list);
+		// fd_redir = open_redir(cmd, tree->type);
+		// free(cmd);
+		// if (fd_redir == -1)
+		// {
+		// 	perror(tree->left->command->token.lexeme);
+		// 	return(1);
+		// }
+		// if (tree->type == REDIRECT_INPUT)
+		// 	dup2(fd_redir, STDIN_FILENO);
+		// else if (tree->type == REDIRECT_OUTPUT || tree->type == REDIRECT_OUTPUT_APPEND)
+		// 	dup2(fd_redir, STDOUT_FILENO);
+		// fd_redir = close_fd(fd_redir);
+		// ret_code = exec_cmd_builtin(tree->right, data);
 	}
 	else
 	{
 		i = 0;
 		args_num = token_list_size(tree->command);
-
 		argv = malloc((args_num + 1) * sizeof(char *));
 		argv[args_num] = NULL;
-
 		while (i < args_num)
 		{
 			argv[i] = expand_string(token_get_node_index(tree->command, i)->token.lexeme, data->envp_list);
 			i++;
 		}
 		cmd = argv[0];
-
-		if (ft_strncmp(cmd, "echo", -1) == 0)
-			ret_code = builtin_echo(argv);
-		else if (ft_strncmp(cmd, "cd", -1) == 0)
-			ret_code = builtin_cd(argv, data);
-		else if (ft_strncmp(cmd, "pwd", -1) == 0)
-			ret_code = builtin_pwd();
-		else if (ft_strncmp(cmd, "export", -1) == 0)
-			ret_code = builtin_export(argv, data);
-		else if (ft_strncmp(cmd, "unset", -1) == 0)
-			ret_code = builtin_unset(argv, &data->envp_list);
-		else if (ft_strncmp(cmd, "env", -1) == 0)
-			ret_code = builtin_env(data);
-		else if (ft_strncmp(cmd, "exit", -1) == 0)
-			ret_code = builtin_exit(argv, data);
+		builtin_call(cmd, argv, data);
 		free_envp(argv);
 	}
 	return (ret_code);
